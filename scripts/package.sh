3c89479bddf1181a804565181a80de879919afdf
David Roussel <david.roussel@bjss.com>
2023-07-28 18:09:18 +0100

    fix packaging

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,25 +6,22 @@ set -u          # Treat unset variables as an error, and immediately exit.
 set -o pipefail # If a command in a pipeline fails, make the whole command fail
 
 export COMPONENT="gully"
-export ROOT_DIR="$(dirname $0)/../../../.."
 
 
 export VERSION POINT_VERSION
-VERSION="$(jq -r '.version' ${ROOT_DIR}/package.json)"
+VERSION="$(jq -r '.version' package.json)"
 POINT_VERSION="${RELEASE_VERSION:-"${VERSION}.${CI_PIPELINE_IID:-0}"}"
 
 echo "==== building ${COMPONENT} files"
 
-tsup --clean src/gully.ts
-chmod +x dist/gully.js
+tsup --clean src/gully.ts --out-dir target/bundle --metafile --target es2021
+chmod +x target/bundle/gully.js
+
+# Set consistant timestamp for determinisitic build output
+LAST_CHANGE_DATE=$(git log -1 --date=format:"%Y%m%d%H%M" --format="%ad")
+touch -t "${LAST_CHANGE_DATE}" target/bundle/*
 
 echo "==== packing ${COMPONENT} files"
 
-yarn lambda-package zip-package \
-  'dist/*.js' \
-  'dist/*.js.map' \
-  --outputDir "target/dist" \
-  --component "${COMPONENT}" \
-  --componentVersion "${POINT_VERSION}" \
-  --exclude '**/__test__/**' \
-  --zipFile "NHSD.PopulationHealth.${COMPONENT}-${POINT_VERSION}.zip"
+mkdir -p target/dist
+zip -X "target/dist/${COMPONENT}-${POINT_VERSION}.zip" target/bundle/*.js
