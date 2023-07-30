52135f8f36262304ffdf9aa808120af45a544603
David Roussel <dave@dvae.net>
2023-07-30 09:07:36 +0100

    use default jest runner

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 const { name: componentName } = require('./package.json');
 
 module.exports = {
-  verbose: true,
   moduleDirectories: ['node_modules'],
   coveragePathIgnorePatterns: ['__test__', 'config.ts'],
   transformIgnorePatterns: ['node_modules'],
   testPathIgnorePatterns: ['/node_modules/', '/target/'],
-  testRunner: 'jest-jasmine2',
   coverageDirectory: `<rootDir>/target/reports/coverage/${componentName}`,
   transform: { '\\.(t|j)sx?$': '@swc/jest' },
   collectCoverageFrom: [
